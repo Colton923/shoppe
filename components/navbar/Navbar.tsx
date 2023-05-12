9e8f02c6243574d49551707579de654736a4f97c
Jeni <ziahrahl@gmail.com>
2023-05-12 17:01:51 -0400

    change layout of navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -41,79 +41,87 @@ const Navbar = () => {
       </div> */}
 
       <div className={styles.stickyNav}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="30"
-          height="30"
-          viewBox="0 0 30 30"
-          fill="#8b0000"
-          stroke="#8b0000"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className={styles.hamburger}
-          onClick={() => {
-            if (!loggedIn) {
-              setIsRegisterOverlay(!isRegisterOverlay)
-            } else {
-              alert('You are already logged in.')
-            }
-          }}
-        >
-          <line
-            className={styles.hamburgerLine}
-            x1="3"
-            y1="12"
-            x2="27"
-            y2="12"
-          ></line>
-          <line className={styles.hamburgerLine} x1="3" y1="6" x2="27" y2="6"></line>
-          <line
-            className={styles.hamburgerLine}
-            x1="3"
-            y1="18"
-            x2="27"
-            y2="18"
-          ></line>
-          <line
-            className={styles.hamburgerLine}
-            x1="3"
-            y1="24"
-            x2="27"
-            y2="24"
-          ></line>
-        </svg>
-        <div className={styles.title}>
-          <h2
-            className={styles.titleText}
+        <div className={styles.fancyText}>
+          <Link
+            className={styles.textLink}
+            href={`mailto:${process.env.NEXT_PUBLIC_EMAIL_ADDRESS}}`}
+          ></Link>
+          <Link
+            className={styles.textLink}
+            href={`tel:${process.env.NEXT_PUBLIC_PHONE_NUMBER}`}
+          ></Link>
+        </div>
+        <div className={styles.bottomRow}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            viewBox="0 0 30 30"
+            fill="#8b0000"
+            stroke="#8b0000"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className={styles.hamburger}
             onClick={() => {
-              window.location.href = '/'
+              if (!loggedIn) {
+                setIsRegisterOverlay(!isRegisterOverlay)
+              } else {
+                alert('You are already logged in.')
+              }
             }}
           >
-            Main St. Shoppe
-          </h2>
-          <h2 className={styles.fancyText}>
-            <Link
-              className={styles.textLink}
-              href={`mailto:${process.env.NEXT_PUBLIC_EMAIL_ADDRESS}}`}
-            ></Link>
-            <Link
-              className={styles.textLink}
-              href={`tel:${process.env.NEXT_PUBLIC_PHONE_NUMBER}`}
-            ></Link>
-          </h2>
-        </div>
-        <div className={styles.hamburger} id="cart">
-          <Image
-            src={CartIcon}
-            alt="Cart Icon"
-            width={35}
-            height={35}
-            className={styles.cartIcon}
-            onClick={() => {
-              setIsCartOverlay(!isCartOverlay)
-            }}
-          />
+            <line
+              className={styles.hamburgerLine}
+              x1="3"
+              y1="12"
+              x2="27"
+              y2="12"
+            ></line>
+            <line
+              className={styles.hamburgerLine}
+              x1="3"
+              y1="6"
+              x2="27"
+              y2="6"
+            ></line>
+            <line
+              className={styles.hamburgerLine}
+              x1="3"
+              y1="18"
+              x2="27"
+              y2="18"
+            ></line>
+            <line
+              className={styles.hamburgerLine}
+              x1="3"
+              y1="24"
+              x2="27"
+              y2="24"
+            ></line>
+          </svg>
+          <div className={styles.title}>
+            <h2
+              className={styles.titleText}
+              onClick={() => {
+                window.location.href = '/'
+              }}
+            >
+              Main St. Shoppe
+            </h2>
+          </div>
+          <div className={styles.hamburger} id="cart">
+            <Image
+              src={CartIcon}
+              alt="Cart Icon"
+              width={35}
+              height={35}
+              className={styles.cartIcon}
+              onClick={() => {
+                setIsCartOverlay(!isCartOverlay)
+              }}
+            />
+          </div>
         </div>
       </div>
     </>
