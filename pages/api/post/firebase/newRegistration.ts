29fd3237cfe098bf06bd6b5a03a44b100ed63eb9
Colton923 <110353948+Colton923@users.noreply.github.com>
2023-05-16 22:17:10 -0400

    email system finished

--- pages/api/post/firebase/newRegistration.ts
+++ pages/api/post/firebase/newRegistration.ts
@@ -1,6 +1,6 @@
 import * as admin from 'firebase-admin'
 import type { FormData } from 'components/register/Register'
-import mail, { MailService } from '@sendgrid/mail'
+import { MailService } from '@sendgrid/mail'
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string)
 
@@ -28,13 +28,59 @@ const handler = async (req: newRegistration, res: any) => {
     from: 'colton923@gmail.com',
     subject: 'New Wholesale Registration',
     text: 'New Wholesale Registration',
-    html: `<p>New Wholesale Registration
-    Company Name: ${formData.businessName}
-    Address: ${formData.address}
-    Email: ${formData.email}
-    Phone: ${formData.phoneNumber}
-    TIN: ${formData.tin}</p>
-    <a href='https://us-central1-main-st-shoppe.cloudfunctions.net/emailHook?email=${formData.email}'>Approve</a>`,
+    html: `<html>
+    <head>
+        <title>New Wholesale Registration</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                background-color: #f2f2f2;
+                margin: 0;
+                padding: 20px;
+            }
+
+            .container {
+                background-color: #ffffff;
+                padding: 20px;
+                border-radius: 5px;
+                box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+            }
+
+            .header {
+                font-size: 24px;
+                font-weight: bold;
+                margin-bottom: 20px;
+            }
+
+            .details {
+                margin-bottom: 10px;
+            }
+
+            .approve-button {
+                display: inline-block;
+                padding: 10px 20px;
+                background-color: #4CAF50;
+                color: #ffffff;
+                text-decoration: none;
+                border-radius: 5px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="header">New Wholesale Registration</div>
+            <div class="details">
+                <p>Company Name: ${formData.businessName}</p>
+                <p>Address: ${formData.address}</p>
+                <p>Email: ${formData.email}</p>
+                <p>Phone: ${formData.phoneNumber}</p>
+                <p>TIN: ${formData.tin}</p>
+            </div>
+            <a class="approve-button" href="https://us-central1-main-st-shoppe.cloudfunctions.net/emailHook?email=${formData.email}">Approve</a>
+        </div>
+    </body>
+    </html>
+    `,
   }
   mailService.send(msg)
 
