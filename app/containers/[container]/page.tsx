869e00ed41d43aee89412247ba46d9456f451730
Colton923 <110353948+Colton923@users.noreply.github.com>
2023-06-26 02:54:39 -0400

    .

--- app/containers/[container]/page.tsx
+++ app/containers/[container]/page.tsx
@@ -1,17 +1,23 @@
-'use client'
+import client from '@lib/sanity/client'
+import queries from '@lib/sanity/queries'
+import * as SanityTypes from 'types/SanityItem'
 import Flavors from '@components/popcorn/flavors/Flavors'
-import { useLocalContext } from '@components/context/LocalContext'
 
-export default function Page() {
-  const { activeContainer } = useLocalContext()
+export async function getContainer(id: string) {
+  return await client.fetch(queries.container(id))
+}
 
-  if (!activeContainer) {
-    return null
+export default async function Page({ params }: { params: { container: string } }) {
+  const { container } = params
+  const contain: SanityTypes.Container[] = await getContainer(container)
+  if (!contain.length) {
+    if (!(contain.length > 0)) {
+      return
+    }
   }
-
   return (
     <>
-      <Flavors container={[activeContainer]} />
+      <Flavors container={contain} />
     </>
   )
 }
